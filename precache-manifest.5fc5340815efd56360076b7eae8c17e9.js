df5fa6c5905dbf2bffaafda4dd79a885d02e8064
dohoons <dohoons@gmail.com>
2018-11-18 14:13:33 +0900

    Updates

---
+++ precache-manifest.5fc5340815efd56360076b7eae8c17e9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ae1b2e3c8e1d4a2d624",
+    "url": "/world/static/js/runtime~main.0ae1b2e3.js"
+  },
+  {
+    "revision": "8e0997339abb58f2171b",
+    "url": "/world/static/js/main.8e099733.chunk.js"
+  },
+  {
+    "revision": "0f547cc7b96df96241c4",
+    "url": "/world/static/js/1.0f547cc7.chunk.js"
+  },
+  {
+    "revision": "bc83542be6eae0737f92a79594be8fad",
+    "url": "/world/index.html"
+  }
+];
