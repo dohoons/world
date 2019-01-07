763c3696f0b73c2b98fdfa1e1dbea481b1edd3f1
dohoons <dohoons@gmail.com>
2019-01-07 10:23:58 +0900

    코드스플리팅 loadable-components 로 교체

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,49 @@
 import React from 'react'
-import Loadable from 'react-loadable'
+import loadable from '@loadable/component'
 import ReactPlaceholder from 'react-placeholder'
 
-const PageLoading = () => <div className="page-loading">
-  <ReactPlaceholder showLoadingAnimation={true} type='text' ready={false} rows={20} color='#E0E0E0'><div></div></ReactPlaceholder>
-</div>
+const PageLoading = (
+  <div className="page-loading">
+    <ReactPlaceholder showLoadingAnimation={true} type='text' ready={false} rows={20} color='#E0E0E0'><div></div></ReactPlaceholder>
+  </div>
+)
 
-export const Home = Loadable({
-  loader: () => import('./Home'),
-  loading: PageLoading
-})
+export const Home = loadable(
+  () => import('./Home'),
+  { fallback: PageLoading }
+)
 
-export const Login = Loadable({
-  loader: () => import('./Login'),
-  loading: PageLoading
-})
+export const Login = loadable(
+  () => import('./Login'),
+  { fallback: PageLoading }
+)
 
-export const Logout = Loadable({
-  loader: () => import('./Logout'),
-  loading: PageLoading
-})
+export const Logout = loadable(
+  () => import('./Logout'),
+  { fallback: PageLoading }
+)
 
-export const Join = Loadable({
-  loader: () => import('./Join'),
-  loading: PageLoading
-})
+export const Join = loadable(
+  () => import('./Join'),
+  { fallback: PageLoading }
+)
 
-export const Form = Loadable({
-  loader: () => import('./Form'),
-  loading: PageLoading
-})
+export const Form = loadable(
+  () => import('./Form'),
+  { fallback: PageLoading }
+)
 
-export const Article = Loadable({
-  loader: () => import('./Article'),
-  loading: PageLoading
-})
+export const Article = loadable(
+  () => import('./Article'),
+  { fallback: PageLoading }
+)
 
-export const Profile = Loadable({
-  loader: () => import('./Profile'),
-  loading: PageLoading
-})
+export const Profile = loadable(
+  () => import('./Profile'),
+  { fallback: PageLoading }
+)
 
-export const ProfileEdit = Loadable({
-  loader: () => import('./ProfileEdit'),
-  loading: PageLoading
-})
+export const ProfileEdit = loadable(
+  () => import('./ProfileEdit'),
+  { fallback: PageLoading }
+)
