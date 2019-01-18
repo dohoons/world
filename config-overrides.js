64114e0b3b7e327637fe12834544189587f4a876
dohoons <dohoons@gmail.com>
2019-01-18 22:45:53 +0900

    webpack-bundle-analyzer 추가

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,20 @@
 const path = require('path')
 const rewireReactHotLoader = require('react-app-rewire-hot-loader')
 const rewireAliases = require('react-app-rewire-aliases')
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
 
 module.exports = function override(config, env) {
   config = rewireReactHotLoader(config, env)
   config = rewireAliases.aliasesOptions({
       '~': path.resolve(__dirname, './src')
   })(config, env)
+  
+  if(env === 'production') {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: 'static',
+      reportFilename: 'report.html'
+    })
+  }
   
   return config
 }
