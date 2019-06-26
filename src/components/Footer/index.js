637a0bb0502c5de4c2959e24b04e7250cfc86e06
dohoons <dohoons@gmail.com>
2019-06-26 20:38:00 +0900

    button type

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -14,8 +14,8 @@ const Footer = () => {
     <CommonFooter>
       <h2 className="hide">{t('heading')}</h2>
       <div className="lang-change">
-        <button className={i18n.language === 'ko' ? 'is-active': ''} onClick={() => changeLanguage('ko')}><i className="fas fa-check"></i> 한국어</button>
-        <button className={i18n.language === 'en' ? 'is-active': ''} onClick={() => changeLanguage('en')}><i className="fas fa-check"></i> English</button>
+        <button type="button" className={i18n.language === 'ko' ? 'is-active': ''} onClick={() => changeLanguage('ko')}><i className="fas fa-check"></i> 한국어</button>
+        <button type="button" className={i18n.language === 'en' ? 'is-active': ''} onClick={() => changeLanguage('en')}><i className="fas fa-check"></i> English</button>
       </div>
     </CommonFooter>
   )
