1b0587f5c45e7f6a55712d39a6b4417bcf0e84cd
dohoons <dohoons@gmail.com>
2019-10-09 22:04:30 +0900

    rewired 수정

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,15 @@
 const path = require('path')
-const rewireReactHotLoader = require('react-app-rewire-hot-loader')
-const rewireAliases = require('react-app-rewire-aliases')
-const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
+const { addWebpackAlias, addBundleVisualizer, override } = require('customize-cra')
+const rewireReactHotLoader = require('react-app-rewire-hot-loader-for-customize-cra')
 
-module.exports = function override(config, env) {
-  config = rewireReactHotLoader(config, env)
-  config = rewireAliases.aliasesOptions({
+module.exports = override(
+  rewireReactHotLoader(),
+  addWebpackAlias({
     'react-dom': '@hot-loader/react-dom',
     '~': path.resolve(__dirname, './src')
-  })(config, env)
-  
-  if(process.env.npm_lifecycle_event === 'build:stats') {
-    config = rewireWebpackBundleAnalyzer(config, env, {
-      analyzerMode: 'static',
-      reportFilename: 'report.html'
-    })
-  }
-  
-  return config
-}
+  }),
+  addBundleVisualizer({
+    analyzerMode: 'static',
+    reportFilename: 'report.html'
+  }, true),
+)
